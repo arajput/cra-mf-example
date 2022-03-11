45d3790283455054ac9d544138f918f76b873f78
Arun Rajput <arunrajput@Aruns-MacBook-Pro-2.local>
2022-03-11 12:36:03 +0530

    core module integrated with teacher app

--- packages/teacher-app/modulefederation.config.js
+++ packages/teacher-app/modulefederation.config.js
@@ -3,6 +3,7 @@ const { dependencies } = require('./package.json');
 module.exports = {
   name: 'host',
   remotes: {
+    core: 'core@[window.appModules.core.url]/remoteEntry.js',
     attendance: 'attendance@[window.appModules.attendance.url]/remoteEntry.js',
   },
   shared: {
